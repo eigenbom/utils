ff5bf185592d9eb90822ec1f1c6d5beaf7766eed
Benjamin Porter <benjamin.porter@gmail.com>
2018-02-19 20:24:41 +1100

    Added array2d.

---
+++ include/array2d.h
@@ -0,0 +1,132 @@
+// Customise the behaviour of array2d by defining these before including it:
+// #define BSP_ARRAY2D_LOG_ALLOCATION(message) to log errors
+
+#ifndef BSP_ARRAY2D_H
+#define BSP_ARRAY2D_H
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#ifdef BSP_ARRAY2D_LOG_ALLOCATION
+#include <typeindex>
+#include <sstream>
+#endif
+
+namespace bsp {
+
+template<typename T> class array2d {
+public:
+    using value_type = T;
+    using reference = T&;
+    using const_reference = const T&;
+    using size_type = int; // Integer
+
+public:
+    array2d() = default;
+    
+    array2d(size_type width, size_type height, const T& value = T()) : width_(width), height_(height), data_(width_ * height_, value), null_value_() {
+        assert(width >= 0 && height >= 0); 
+    }
+
+    array2d(const array2d& other):width_(other.width_), height_(other.height_), data_(other.data_), null_value_(other.null_value_){}
+
+    array2d& operator=(const array2d& other){
+        data_ = other.data_;
+        width_ = other.width_;
+        height_ = other.height_;
+        null_value_ = other.null_value_;
+        return *this;
+    }
+
+    array2d(array2d&& other):width_(other.width_), height_(other.height_), data_(std::move(other.data_)), null_value_(other.null_value_){
+        other.width_ = 0;
+        other.height_ = 0;
+    }
+
+    array2d& operator=(array2d&& other){
+        data_ = std::move(other.data_);
+        width_ = other.width_;
+        height_ = other.height_;
+        null_value_ = other.null_value_;
+        other.width_ = 0;
+        other.height_ = 0;
+        return *this;
+    }
+
+    inline size_type width() const { return width_; }
+
+    inline size_type height() const { return height_; }
+
+    void resize(size_type width, size_type height, const T& value = T()){
+        assert(width >= 0 && height >= 0);
+        
+    #ifdef BSP_ARRAY2D_LOG_ALLOCATION
+        int megabytes = (sizeof(T) * width * height) / (1024 * 1024);
+        if (megabytes >= 1){
+            std::ostringstream oss;
+            oss << "Resizing array2d<" << typeid(value).name() << ">"
+                << "(" << width << "x" << height << ") " 
+                << megabytes << "M";
+            BSP_ARRAY2D_LOG_ALLOCATION(oss.str());
+        }
+    #endif
+
+        width_ = width;
+        height_ = height;
+        data_.resize(width_ * height_);
+        fill(value);
+        null_value_ = T();
+    }
+
+    inline void fill(const T& t) { 
+        std::fill(data_.begin(), data_.end(), t); 
+    }
+
+    inline reference operator()(size_type i, size_type j) { 
+        return valid_index(i, j) ? data_[index(i,j)] : null_value_; 
+    }
+    
+    inline const_reference operator()(size_type i, size_type j) const { 
+        return valid_index(i, j) ? data_[index(i,j)] : null_value_; 
+    }
+    
+    inline bool valid_index(size_type i, size_type j) const {
+        return i >= 0 && i < width_ && j >= 0 && j < height_; 
+    }
+    
+protected:
+    size_type width_  = 0;
+    size_type height_ = 0;
+    std::vector<T> data_;
+    T null_value_;
+
+protected:
+    inline size_type index(size_type i, size_type j) const { return i + j*width_; }
+
+	template<typename T_>
+	friend std::ostream& operator<<(std::ostream&, const array2d<T_>&);
+};
+
+template<typename T>
+inline std::ostream& operator<<(std::ostream& out, const array2d<T>& array) {
+    using size_type = typename array2d<T>::size_type;
+
+	out << "array2d {";
+    for (size_type row = 0; row < array.height_; row++){
+        out << "{";
+        for (size_type col = 0; col < array.width_; col++){
+            out << array(col, row);
+            if (col < array.width_ - 1) out << ", ";
+        }
+        out << "}";
+        if (row < array.height_ - 1) out << ", ";
+    }
+    out << "}";
+	return out;
+}
+
+}
+
+#endif
---
+++ tests/array2d.cpp
@@ -0,0 +1,73 @@
+#include "../include/array2d.h"
+#include "catch.hpp"
+#include "container_matcher.h"
+
+using bsp::array2d;
+using Catch::Equals;
+
+TEST_CASE("array2d basics", "[array2d]") {
+    array2d<int> arr { 2, 2, 42 };
+    CHECK(arr.width() == 2);
+    CHECK(arr.height() == 2);
+    CHECK(arr(0,0) == 42);
+    CHECK(arr(1,0) == 42);
+    CHECK(arr(0,1) == 42);
+    CHECK(arr(1,1) == 42);
+    CHECK(arr(-1,0) == 0);
+
+    arr.resize(4, 2, 42);
+    CHECK(arr.width() == 4);
+    CHECK(arr.height() == 2);    
+    CHECK(arr(0,0) == 42);
+}
+
+TEST_CASE("array2d construction", "[array2d]") {
+    SECTION("default construction"){
+        array2d<int> arr;
+        CHECK(arr.width() == 0);
+        CHECK(arr.height() == 0);
+    }
+
+    SECTION("copy construction"){
+        array2d<int> arr {8, 8, 0};
+        array2d<int> arr2 {arr};
+        CHECK(arr2.width() == 8);
+        CHECK(arr2.height() == 8);
+    }
+
+    SECTION("copy assignment"){
+        array2d<int> arr {8, 8, 0};
+        array2d<int> arr2;
+        arr2 = arr;
+        CHECK(arr2.width() == 8);
+        CHECK(arr2.height() == 8);
+    }
+
+    SECTION("move construction"){
+        array2d<int> arr {8, 8, 0};
+        array2d<int> arr2 {std::move(arr)};
+        CHECK(arr2.width() == 8);
+        CHECK(arr2.height() == 8);
+        CHECK(arr.width() == 0);
+        CHECK(arr.height() == 0);
+    }
+
+    SECTION("move assignment"){
+        array2d<int> arr {8, 8, 0};
+        array2d<int> arr2;
+        arr2 = std::move(arr);
+        CHECK(arr2.width() == 8);
+        CHECK(arr2.height() == 8);
+        CHECK(arr.width() == 0);
+        CHECK(arr.height() == 0);
+    }
+}
+
+
+TEST_CASE("array2d operator<<", "[array2d]") {
+    array2d<int> identity { 2, 2 };
+    identity(0, 0) = 1;
+    identity(1, 1) = 1;
+    std::cout << "Should print the identity array2d...\n";
+    std::cout << identity << "\n";
+}
